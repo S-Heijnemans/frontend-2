2c14c1857ded7a283785f72966e4cb6040963ccf
S-Heijnemans <153827872+S-Heijnemans@users.noreply.github.com>
2025-05-14 16:08:16 +0200

    add firebase to the project

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.emv.apiKey,
+  authDomain: process.emv.authDomain,
+  projectId: process.emv.projectId,
+  storageBucket: process.emv.storageBucket,
+  messagingSenderId: process.emv.messagingSenderId,
+  appId: process.emv.appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
