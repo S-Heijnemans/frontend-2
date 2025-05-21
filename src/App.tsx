f454c7ed7d706a9b7bbb667713b6cb5c55019f49
Renspeters <1195839@student.roc-nijmegen.nl>
2025-05-21 15:42:20 +0200

    install packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ function App() {
 
   return (
     <>
-      <div className="w-[200px] h-[200px] flex items-center justify-center bg-amber-600">
-        <div className="w-[50px] h-[50px] bg-blue-500">
-
-        </div>
+      <div className="w-full h-[90px] flex flex-row items-center overflow-hidden justify-start p-[45px] gap-[120px] m-4 rounded-[10px] bg-[#ebdb99] text-white border-size-[1px] border-solid border-4 border-[#8b5300]" style={{ backgroundImage: 'repeating-linear-gradient(0deg, rgba(255,255,255,0.1) 0px, rgba(255,255,255,0.1) 2px, rgba(255,255,255,0.4) 2px, rgba(255,255,255,0.4) 4px)' }}>
       </div>
       <p>HELLO WORLD</p>
 
